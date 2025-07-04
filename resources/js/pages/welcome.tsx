027fd68e7cdacf955fd039420f33dfa86b4d5062
Simon Vrachliotis <simon@simonswiss.com>
2025-07-04 15:45:24 +1000

    In-house puppy data

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,22 +1,31 @@
-import { Container } from "@/components/Container";
-import { Header } from "@/components/Header";
-import { NewPuppyForm } from "@/components/NewPuppyForm";
-import { PageWrapper } from "@/components/PageWrapper";
-import { PuppiesList } from "@/components/PuppiesList";
-import { Search } from "@/components/Search";
-import { Shortlist } from "@/components/Shortlist";
+import { Container } from '@/components/Container';
+import { Header } from '@/components/Header';
+import { NewPuppyForm } from '@/components/NewPuppyForm';
+import { PageWrapper } from '@/components/PageWrapper';
+import { PuppiesList } from '@/components/PuppiesList';
+import { Search } from '@/components/Search';
+import { Shortlist } from '@/components/Shortlist';
 
-import { LoaderCircle } from "lucide-react";
-import { Suspense, use, useState } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { getPuppies } from "@/queries";
-import { Puppy } from "@/types";
+import { getPuppies } from '@/queries';
+import { Puppy } from '@/types';
+import { LoaderCircle } from 'lucide-react';
+import { Suspense, use, useState } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
-export default function App() {
+export default function App({ puppies }: { puppies: Puppy[] }) {
     return (
         <PageWrapper>
             <Container>
                 <Header />
+                <ul className="mt-4 flex flex-wrap gap-4">
+                    {puppies.map((puppy) => (
+                        <li className="flex gap-2 bg-white p-6 ring ring-black/10">
+                            <img src={puppy.image_url} alt={puppy.name} className="size-24 object-cover" />
+                            <h2>{puppy.name}</h2>
+                        </li>
+                    ))}
+                </ul>
+
                 <ErrorBoundary
                     fallbackRender={({ error }) => (
                         <div className="mt-12 bg-red-100 p-6 shadow ring ring-black/5">
@@ -45,23 +54,16 @@ const puppyPromise = getPuppies();
 
 function Main() {
     const apiPuppies = use(puppyPromise);
-    const [searchQuery, setSearchQuery] = useState("");
+    const [searchQuery, setSearchQuery] = useState('');
     const [puppies, setPuppies] = useState<Puppy[]>(apiPuppies);
 
     return (
         <main>
             <div className="mt-24 grid gap-8 sm:grid-cols-2">
-                <Search
-                    searchQuery={searchQuery}
-                    setSearchQuery={setSearchQuery}
-                />
+                <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
                 <Shortlist puppies={puppies} setPuppies={setPuppies} />
             </div>
-            <PuppiesList
-                puppies={puppies}
-                setPuppies={setPuppies}
-                searchQuery={searchQuery}
-            />
+            <PuppiesList puppies={puppies} setPuppies={setPuppies} searchQuery={searchQuery} />
             <NewPuppyForm puppies={puppies} setPuppies={setPuppies} />
         </main>
     );
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Models\Puppy;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return Inertia::render('welcome', [
+        'puppies' => Puppy::all()->load(['user']),
+    ]);
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
@@ -13,5 +16,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
