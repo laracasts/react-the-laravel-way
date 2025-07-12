8cb14a16a834f285f6e179b0ad01ae30887d08a9
Simon Vrachliotis <simon@simonswiss.com>
2025-07-12 14:02:53 +1000

    Liked Puppies with Inertia Data

--- resources/js/components/LikeToggle.tsx
+++ resources/js/components/LikeToggle.tsx
@@ -1,41 +1,32 @@
-import { Dispatch, SetStateAction, useState } from "react";
-import { Heart, LoaderCircle } from "lucide-react";
-import { Puppy } from "../types";
-import { toggleLikedStatus } from "../queries";
+import { Link, usePage } from '@inertiajs/react';
+import clsx from 'clsx';
+import { Heart, LoaderCircle } from 'lucide-react';
+import { useState } from 'react';
+import { Puppy, SharedData } from '../types';
 
-export function LikeToggle({
-  puppy,
-  setPuppies,
-}: {
-  puppy: Puppy;
-  setPuppies: Dispatch<SetStateAction<Puppy[]>>;
-}) {
-  const [pending, setPending] = useState(false);
-  return (
-    <button
-      className="group"
-      onClick={async () => {
-        setPending(true);
-        const updatedPuppy = await toggleLikedStatus(puppy.id);
-        setPuppies((prevPups) => {
-          return prevPups.map((existingPuppy) =>
-            existingPuppy.id === updatedPuppy.id ? updatedPuppy : existingPuppy,
-          );
-        });
-        setPending(false);
-      }}
-    >
-      {pending ? (
-        <LoaderCircle className="animate-spin stroke-slate-300" />
-      ) : (
-        <Heart
-          className={
-            puppy.likedBy.includes(1)
-              ? "fill-pink-500 stroke-none"
-              : "stroke-slate-200 group-hover:stroke-slate-300"
-          }
-        />
-      )}
-    </button>
-  );
+export function LikeToggle({ puppy }: { puppy: Puppy }) {
+    const [pending, setPending] = useState(false);
+    const { auth } = usePage<SharedData>().props;
+
+    return (
+        <Link
+            preserveScroll
+            method="patch"
+            href={route('puppies.like', puppy.id)}
+            className={clsx('group', !auth.user && 'cursor-not-allowed')}
+            disabled={!auth.user}
+        >
+            {pending ? (
+                <LoaderCircle className="animate-spin stroke-slate-300" />
+            ) : (
+                <Heart
+                    className={
+                        auth.user && puppy.likedBy.includes(auth.user.id)
+                            ? 'fill-pink-500 stroke-none'
+                            : 'stroke-slate-200 group-hover:stroke-slate-300'
+                    }
+                />
+            )}
+        </Link>
+    );
 }
