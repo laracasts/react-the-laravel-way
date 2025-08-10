e2089fed207e60aa0992b439043084d8869dee06
Simon Vrachliotis <simon@simonswiss.com>
2025-08-11 09:08:04 +1000

    Redirect to page 1 of pagination on new puppy creation

--- app/Http/Controllers/PuppyController.php
+++ app/Http/Controllers/PuppyController.php
@@ -97,6 +97,6 @@ class PuppyController extends Controller
         ]);
 
         // Redirect to the same page
-        return back()->with('success', 'Puppy created successfully!');
+        return redirect()->route('home', ['page' => 1])->with('success', 'Puppy created successfully!');
     }
 }
